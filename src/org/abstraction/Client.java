16442c53a7fac3e321c7471dd53083d8fcf8855f
Trickz7112 <manikandan7112@gmail.com>
2022-01-22 12:54:09 +0530

    FirstGIT

---
+++ src/org/abstraction/Client.java
@@ -0,0 +1,15 @@
+package org.abstraction;
+
+public abstract class Client extends Employee
+{
+	public void clientId() {
+System.out.println("fas");
+	}
+
+	@Override
+	public void empid() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
